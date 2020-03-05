632f148f168ea0ca2d0767d3306396aa15b9006a
Rodrigo de Souza <rsouza01@gmail.com>
2020-03-05 22:13:30 +0100

    Removing tasks from the routes file

--- src/routes.py
+++ src/routes.py
@@ -7,76 +7,7 @@ from flask import request
 from flask import url_for
 from flask_httpauth import HTTPBasicAuth
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
-
-
-def make_public_task(task):
-    new_task = {}
-    for field in task:
-        if field == 'id':
-            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
-        else:
-            new_task[field] = task[field]
-    return new_task
-
-def get_tasks():
-    return jsonify({'tasks': [make_public_task(task) for task in tasks]})
-
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    return jsonify({'task': make_public_task(task[0])})
-
-def create_task():
-    if not request.json or not 'title' in request.json:
-        abort(400)
-    task = {
-        'id': tasks[-1]['id'] + 1,
-        'title': request.json['title'],
-        'description': request.json.get('description', ""),
-        'done': False
-    }
-    tasks.append(task)
-    return jsonify({'task': make_public_task(task)}), 201
-
-def update_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    if not request.json:
-        abort(400)
-    if 'title' in request.json and type(request.json['title']) != unicode:
-        abort(400)
-    if 'description' in request.json and type(request.json['description']) is not unicode:
-        abort(400)
-    if 'done' in request.json and type(request.json['done']) is not bool:
-        abort(400)
-    task[0]['title'] = request.json.get('title', task[0]['title'])
-    task[0]['description'] = request.json.get('description', task[0]['description'])
-    task[0]['done'] = request.json.get('done', task[0]['done'])
-    return jsonify({'task': make_public_task(task[0])})
-
-def delete_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-    if len(task) == 0:
-        abort(404)
-    tasks.remove(task[0])
-    return jsonify({'result': True})
+import task_handler as task_handler
 
 
 def not_found(error):
@@ -84,12 +15,14 @@ def not_found(error):
 
 
 def register_endpoints(app):
-    app.add_url_rule('/todo/api/v1/tasks', 'get_tasks', view_func=get_tasks, methods=['GET'])
-    app.add_url_rule('/todo/api/v1/tasks/<int:task_id>', 'get_task', view_func=get_task, methods=['GET'])
 
-    app.add_url_rule('/todo/api/v1/tasks', 'create_task', view_func=create_task, methods=['POST'])
-    app.add_url_rule('/todo/api/v1/tasks/<int:task_id>', 'update_task', view_func=update_task, methods=['PUT'])
-    app.add_url_rule('/todo/api/v1/tasks/<int:task_id>', 'delete_task', view_func=delete_task, methods=['DELETE'])
+
+    app.add_url_rule('/todo/api/v1/tasks', 'get_tasks', view_func=task_handler.get_tasks, methods=['GET'])
+    app.add_url_rule('/todo/api/v1/tasks/<int:task_id>', 'get_task', view_func=task_handler.get_task, methods=['GET'])
+
+    app.add_url_rule('/todo/api/v1/tasks', 'create_task', view_func=task_handler.create_task, methods=['POST'])
+    app.add_url_rule('/todo/api/v1/tasks/<int:task_id>', 'update_task', view_func=task_handler.update_task, methods=['PUT'])
+    app.add_url_rule('/todo/api/v1/tasks/<int:task_id>', 'delete_task', view_func=task_handler.delete_task, methods=['DELETE'])
 
     app._register_error_handler(None, 404, not_found)
 
