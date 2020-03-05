dabea87ea22007fe68e810fe6632bd4dde694a9b
Rodrigo de Souza <rsouza01@gmail.com>
2020-03-05 22:09:33 +0100

    Refactoring

--- src/app.py
+++ src/app.py
@@ -2,30 +2,11 @@
 
 from flask import Flask, jsonify
 
+import routes as routes
+
 app = Flask(__name__)
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web', 
-        'done': False
-    }
-]
-
-@app.route('/')
-def hello_world():
-    return 'hello world!'
-
-@app.route('/todo/api/v1/tasks', methods=['GET'])
-def get_tasks():
-    return jsonify({'tasks': tasks})
+routes.register_endpoints(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
