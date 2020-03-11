7aa8eb75c917cfb7803d7c60ce234f8db9a99140
Rodrigo de Souza <rsouza01@gmail.com>
2020-03-11 12:46:52 +0100

    Syntax error fixed

--- astroimages_api/util/file_system.py
+++ astroimages_api/util/file_system.py
@@ -1,6 +1,5 @@
-import os
+
 
 # List all files in a directory using os.listdir
-
-def list_files_in_folder(basepath)
-  return (entry for entry in basepath.iterdir() if entry.is_file())
+def list_files_in_folder(basepath):
+    return (entry for entry in basepath.iterdir() if entry.is_file())
