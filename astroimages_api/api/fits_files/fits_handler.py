5bfe46d29c1f8a0e933d5ea2ec642845af25c5e2
Rodrigo de Souza <rsouza01@gmail.com>
2020-03-22 15:31:03 +0100

    Handlers updated with tests

--- astroimages_api/api/fits_files/fits_handler.py
+++ astroimages_api/api/fits_files/fits_handler.py
@@ -1,31 +1,11 @@
 import os
 
-# from flask import abort
-# from flask import url_for
+from flask import abort
 from flask import jsonify
 
 # from astroimages_api.util.file_system import list_files_in_folder
-from astroimages_fits.fits_util_functions import extract_metadata_from_fits_file
+# from astroimages_fits.fits_util_functions import extract_metadata_from_fits_file
 from astroimages_api.api.fits_files.fits_service import FitsFileService
-
-
-# def make_public_fits_file(fits_file):
-#     new_fits_file = {}
-#     for field in fits_file:
-#         if field == 'id':
-#             new_fits_file['id'] = fits_file['id']
-#             new_fits_file['uri'] = url_for('get_fits_file', fits_file_id=fits_file['id'], _external=True)
-#         else:
-#             new_fits_file[field] = fits_file[field]
-#     return new_fits_file
-
-
-# def get_fits_files_from_folder():
-#     folder = os.environ['FITS_FOLDER']
-#     fitsFileService = FitsFileService(folder)
-#     fits_file_names = fitsFileService.get_fits_files_from_folder()
-
-#     return [extract_metadata_from_fits_file(fits_file_name) for fits_file_name in fits_file_names]
 
 
 def get_fits_files():
@@ -34,24 +14,25 @@ def get_fits_files():
 
     return jsonify(
         {
-            'fits_files': 
-            # [ make_public_fits_file(fits_file) for fits_file in fits_files]
-                fits_files
+            'fits_files': fits_files
         }
     )
 
 
-# def get_fits_file(fits_file_id):
-#     fits_files = get_fits_files_from_folder()
-#     fits_file = [fits_file for fits_file in fits_files if fits_file['id'] == fits_file_id]
-#     if len(fits_file) == 0:
-#         abort(404)
+def get_fits_file(fits_file_id):
+    fitsFileService = FitsFileService(os.environ['FITS_FOLDER'])
+    fits_files = fitsFileService.get_fits_files()
 
-#     return jsonify(
-#         {
-#             'fits_file': make_public_fits_file(fits_file[0])
-#         }
-#     )
+    fits_file = [fits_file for fits_file in fits_files if fits_file['id'] == fits_file_id]
+
+    if len(fits_file) == 0:
+        abort(404)
+
+    return jsonify(
+        {
+            'fits_file': fits_file
+        }
+    )
 
 
 def register_endpoints(api_endpoint, app, api):
@@ -59,4 +40,4 @@ def register_endpoints(api_endpoint, app, api):
     method_url = f'{api_endpoint}/{version}/fits-files'
 
     app.add_url_rule(f'{method_url}', 'get_fits_files', view_func=get_fits_files, methods=['GET'])
-    # app.add_url_rule(f'{method_url}/<int:fits_file_id>', 'get_fits_file', view_func=get_fits_file, methods=['GET'])
+    app.add_url_rule(f'{method_url}/<int:fits_file_id>', 'get_fits_file', view_func=get_fits_file, methods=['GET'])
--- test/unit/api/fits_files/test_fits_handler.py
+++ test/unit/api/fits_files/test_fits_handler.py
@@ -1,15 +1,17 @@
 import unittest
-import json
-from flask import current_app
+
+from werkzeug.exceptions import HTTPException
 
 import astroimages_api.api.fits_files.fits_handler as fits_handler
 import astroimages_api.server as server
+
 
 class TestFitsFileHandler(unittest.TestCase):
 
     # executed prior to each test
     def setUp(self):
-        pass
+        self.should_print = False
+        server.rest_app.config["SERVER_NAME"] = 'testlocalhost'
 
     # executed after each test
     def tearDown(self):
@@ -18,25 +20,49 @@ class TestFitsFileHandler(unittest.TestCase):
     def test_get_fits_files(self):
         "Testing the get_fits_files - Happy path."
 
+        json_data = {}
+
         with server.rest_app.test_client():
             with server.rest_app.app_context():
                 response = fits_handler.get_fits_files()
                 json_data = response.get_json()
-                print('RESPONSE = %s' % json_data['fits_files'])
 
-    # def test_get_fits_file(self):
-    #     "Testing the get_fits_file - Happy path."
-    #     response = fits_handler.get_fits_file(12)
-    #     print('RESPONSE = %s' + response)
+        if self.should_print:
+            print('RESPONSE = %s' % json_data['fits_files'])
 
-    # def test_list(self):
-    #     print('\n')
-    #     response = self.flask_app.get(self.BASE_URL)
-    #     print(str(response))
-        # data = json.loads(response.get_data())
-        # print(data)
-        # if data is not None:
-        #     self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(json_data['fits_files']), 2)
+
+    def test_get_fits_file(self):
+        "Testing the get_fits_file - Happy path."
+
+        hash = 'a59d38bc2bfe0a71c54ce366233997b1'
+
+        json_data = {}
+
+        with server.rest_app.test_client():
+            with server.rest_app.app_context():
+                response = fits_handler.get_fits_file(fits_file_id=hash)
+                json_data = response.get_json()
+
+        if self.should_print:
+            print('RESPONSE = %s' % json_data['fits_file'])
+
+        self.assertEqual(len(json_data['fits_file']), 1)
+
+    def test_get_fits_file_not_found(self):
+        "Testing the get_fits_file - File non existent."
+
+        hash = 'SHOULD_NOT_BE_FOUND'
+        error_code = 0
+
+        try:
+            with server.rest_app.test_client():
+                with server.rest_app.app_context():
+                    fits_handler.get_fits_file(fits_file_id=hash)
+        except HTTPException as exception:
+            error_code = exception.code
+
+        self.assertEqual(error_code, 404)
 
 
 if __name__ == '__main__':
