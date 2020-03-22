00f2808c228fa9c743d07c4234566e65202a4919
Rodrigo de Souza <rsouza01@gmail.com>
2020-03-22 18:45:18 +0100

    Fixing tests - first commit

--- test/unit/api/fits_files/test_fits_service.py
+++ test/unit/api/fits_files/test_fits_service.py
@@ -1,28 +1,36 @@
 import unittest
 import hashlib
-import json
+import ntpath
 
 from astroimages_api.api.fits_files.fits_service import FitsFileService
+import astroimages_fits.fits_test_util_functions as ftuf
 
 
 class TestFitsFileService(unittest.TestCase):
 
     def setUp(self):
-        folder = '/home/rsouza/Projects/AstroImages/FITS_FOLDER'
-        self.fitsFileService = FitsFileService(folder)
+        self.total_files_per_folder = 2
+        self.folder_handler, self.files_list = ftuf.create_empty_fits_files_on_temp_folder(self.total_files_per_folder)
+
+        # print(self.files_list)
+        # input("Press Enter to continue...")
+
+        self.fitsFileService = FitsFileService(self.folder_handler.name)
+
+    def tearDown(self):
+        self.folder_handler.cleanup()
 
     def test_get_fits_files(self):
         files = self.fitsFileService.get_fits_files()
-        self.assertEqual(len(files), 2)
+        self.assertEqual(len(files), self.total_files_per_folder)
 
     def test_get_fits_file(self):
-        fileName = b'/home/rsouza/Projects/AstroImages/FITS_FOLDER/FOCx38i0101t_c0f.fits'
+        fileName = self.files_list[0].encode('utf-8')
 
         file = self.fitsFileService.get_fits_file(hashlib.md5(fileName).hexdigest(), True)
 
         self.assertNotEqual(file, None)
-        self.assertEqual(file['title'], 'FOCx38i0101t_c0f.fits')
-        self.assertEqual(file['primaryHDU']['BITPIX'], -32)
+        self.assertEqual(file['title'], ntpath.basename(self.files_list[0]))
 
 
 if __name__ == '__main__':
