0e4b2e081e150bd1dbe02d18350bec244e8942ea
Rodrigo de Souza <rsouza01@gmail.com>
2020-03-24 21:29:56 +0100

    Including new dependency

--- server.sh
+++ server.sh
@@ -2,7 +2,7 @@
 
 clear
 
-export PYTHONPATH=$PYTHONPATH:../astroimages-fits/
+export PYTHONPATH=$PYTHONPATH:../astroimages-fits/:../astroimages-file-drivers
 export FITS_FOLDER=${PWD%/*}/FITS_FOLDER/
 
 ./astroimages_api/server.py
